a7a2744591fc1cd1bcf6440d4b7dc440b4df6a11
Le-TRex <clarembo@gmail.com>
2021-02-04 14:41:26 +0100

    ajout du favicon

--- lamp/cours/resources/views/Layouts/Front.blade.php
+++ lamp/cours/resources/views/Layouts/Front.blade.php
@@ -9,7 +9,7 @@
     <title>Progressus - Free business bootstrap template by GetTemplate</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <link rel="shortcut icon" href="assets/images/gt_favicon.png">
+    <link rel="shortcut icon" href="{{ asset('images/gt_favicon.png') }}">
 
     <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
