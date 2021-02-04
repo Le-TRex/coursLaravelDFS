459906e2f2cd9056921f3b7869f02424c0912298
Le-TRex <clarembo@gmail.com>
2021-02-04 16:25:56 +0100

    exercice HS à cause de profilE

--- lamp/cours/app/Http/Controllers/ProfilController.php
+++ lamp/cours/app/Http/Controllers/ProfilController.php
@@ -9,8 +9,13 @@ class ProfilController extends Controller
     public function Profil(){
         return view('profil');
     }
-    
+
     public function PostProfil(){
         return view('profil');
+    }
+    public function PostProfil (Request $request) {
+        $prenom = $request->prenom;
+        $email = $request->email;
+        return view('profil', ["email" => email, "prenom" => prenom ]);
     }
 }
--- lamp/cours/resources/views/profil.blade.php
+++ lamp/cours/resources/views/profil.blade.php
@@ -1,14 +1,26 @@
 @extends('.Layouts.Front')
 
 @section('content')
-<a href="{{route('home')}}">Home</a>
+
+    <a href="{{route('home')}}">Home</a>
+
+    <h1>TUT TUT POUET POUET LA VOILÀÀÀ ! LA TOTOMOBILEEEUUUH !!</h1>
+
+    @if (isset($prenom))
+        {{ $prenom }}
+        {{ $email }}
+    @endif
+
     <form method="post" action="{{ route('PostProfil') }}">
+
         @csrf
+
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input type="email" class="form-control"  name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
+
         <div class="form-group">
             <label for="exampleInputPassword1">prénom</label>
             <input type="text" class="form-control" name="prenom" id="exampleInputPassword1" placeholder="Prenom">
@@ -16,5 +28,19 @@
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+
+{{--    <form method="POST" action="/">--}}
+{{--        @csrf--}}
+{{--        @method('POST')--}}
+
+{{--        <label for="firstName">firstName</label>--}}
+{{--        <input id="firstName" type="text">--}}
+
+{{--        <label for="lastName">firstName</label>--}}
+{{--        <input id="lastName" type="text">--}}
+
+{{--        <input type="submit" value="Envoyer ! ">--}}
+
+{{--    </form>--}}
 
 @endsection
