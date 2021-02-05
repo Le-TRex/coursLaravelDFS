60819339739ff9f5f188cd7e1b61aee4a64653a7
Le-TRex <clarembo@gmail.com>
2021-02-05 11:12:22 +0100

    création DB todos + ajout de lignes

--- lamp/cours/app/Http/Controllers/HomeController.php
+++ lamp/cours/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -24,6 +25,10 @@ class HomeController extends Controller
      * @return Application|Factory|View
      */
     public function Home(){
+        $todo = new Todo();
+        $todo->name = "salut";
+        $todo->description = "je suis une description";
+        $todo->save();
         return view('welcome');
     }
 }
