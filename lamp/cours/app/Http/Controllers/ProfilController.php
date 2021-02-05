4aff53b507858483d7b2f65bb4ba2fc2b93afe73
Le-TRex <clarembo@gmail.com>
2021-02-05 15:40:50 +0100

    accolade manquante corrigée

--- lamp/cours/app/Http/Controllers/ProfilController.php
+++ lamp/cours/app/Http/Controllers/ProfilController.php
@@ -20,5 +20,5 @@ class ProfilController extends Controller
 
 //    public function PostProfil(ProfilRequest $request){
 //        return view('profil', [ "email" => $request->email, "prenom" => $request->prenom ] );
-//    }
+    }
 }
